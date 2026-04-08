739cdbf25d66e8d812f7aaa5ad6431015fa51e60
Sergio <sergiodfc@gmail.com>
2026-04-07 20:51:44 -0500

    Initial commit

---
+++ src/file-01.py
@@ -0,0 +1,3 @@
+#Este es un archivo de prueba file-01.py
+#Esta es una línea adicional
+#Linea abc
---
+++ src/file-02.py
@@ -0,0 +1,4 @@
+#Este es un archivo de prueba file-02.py
+#Esta es otra línea adicional
+#Linea xyz
+#Linea 123
